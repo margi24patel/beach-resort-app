f8997684d5eed67c6eb635a0834b3e99931f3ec6
Margi Patel <margipatel@Margis-MacBook-Air.local>
2020-10-14 20:04:40 -0400

    modify browser router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 // import './index.css';
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { RoomProvider } from "./context";
 
 ReactDOM.render(
   <RoomProvider>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </RoomProvider>,
   document.getElementById("root")
 );
